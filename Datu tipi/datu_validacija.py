5ff48d17ed943fa2818f0cd3f01ed9610ecdc1b5
Valerijs <valerafhjhg@gmail.com>
2026-04-23 09:19:17 +0300

    Datu_validācija

---
+++ Datu tipi/datu_validacija.py
@@ -0,0 +1,19 @@
+#23/04/2026
+
+#Datu validacija
+
+#Datu ievade
+vecums = input("Ievadi vecumu: ")
+print(vecums)
+
+if vecums.isdigit(): #.isdigit()metode pārbauda, vai saņemtais lielums ir skaitlis vai nav
+    print ("Ir skaitlis")
+      if int (vecums) >=18: 
+        print("Tu esi pilngadīgs.")#Izpildās, ja nosacījums ir patiess
+    else:
+        print("Tu vēl neesi pilngadīgs.")#Izpildās, ja nosacījums nav patiess
+else:
+    print("Nav skaitlis")
+
+
+
