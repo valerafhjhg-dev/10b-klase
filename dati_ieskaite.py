088a42ee29d99a81aa273ba63ed6ade15d69f306
Valerijs <valerafhjhg@gmail.com>
2026-05-19 09:07:40 +0300

    dati_ieskaite.csv

--- dati_ieskaite.py
+++ dati_ieskaite.py
@@ -15,3 +15,20 @@ with open("dati_ieskaite.csv","w",encoding="utf-8") as f:
     f.write("Jānis,18\n")
     f.write("Laura,20\n")
     f.write("Marta,15\n")
+
+with open("dati_ieskaite.csv", encoding="utf-8") as f:
+    next(f) 
+    for rinda in f:
+        vards,vecums = rinda.strip().split(",")
+        if int(vecums) >= 18:
+            print(vards)
+
+with open("dati_ieskaite.csv", encoding="utf-8") as f:
+    next(f)
+    vecumi = []
+    for rinda in f:
+        _, vecums = rinda.strip().split(",")
+        vecumi.append(int(vecums))
+
+    videjais_vecums = sum(vecumi) / len(vecumi)
+    print(f"Vidējais vecums: {videjais_vecums}")
